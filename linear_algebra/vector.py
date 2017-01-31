7f72d6eec8ff37af1d7fa3c4f9fc0e7b8854d1e6
lauryndbrown <lauryndbrown@gmail.com>
2017-01-31 00:27:38 -0600

    Added direction and magnitude methods. removed a lot of decimal precision leaving rounding up to the user.

--- linear_algebra/vector.py
+++ linear_algebra/vector.py
@@ -15,29 +15,46 @@ class Vector:
         except TypeError:
             raise TypeError('The coordinates must be iterable')
     def plus(self, vector):
-        sums = tuple([ float(Decimal(str(a))+Decimal(str(b))) for a, b in zip(self.coordinates, vector.coordinates) ])
+        sums = tuple([a+b for a, b in zip(self.coordinates, vector.coordinates) ])
         return Vector(sums)
         
     def minus(self, vector):
-        differences = tuple([float(Decimal(str(a))-Decimal(str(b))) for a, b in zip(self.coordinates, vector.coordinates) ])
+        differences = tuple([ a-b for a, b in zip(self.coordinates, vector.coordinates) ])
         return Vector(differences)
 
     def scalar_multiply(self, scalar):
         if isinstance(scalar, collections.Iterable):
             raise TypeError('value must not be iterable')
-        products = tuple([float(Decimal(coordinate)*Decimal(scalar)) for coordinate in self.coordinates ])
+        products = tuple([coordinate*scalar for coordinate in self.coordinates ])
         return Vector(products)
 
+    def magnitude(self):
+        """Returns the magnitude of a Vector
+            Using the pathagorean theorem
+        """
+        sum_squares = 0
+        for coord in self.coordinates:
+            sum_squares+= coord**2
+        return sum_squares**0.5
+
+    def direction(self):
+        """Returns the direction of the function
+            Unit vector in the direction of the vector
+        """
+        try:
+            return self.scalar_multiply(self.magnitude()**-1)
+        except ZeroDivisionError:
+            raise ZeroDivisionError('zero vector has no direction')
     def round_coordinates(self, precision):
         """Round coordinates in a vector
            Uses decimal.ROUND_HALF_UP
         """
         if isinstance(precision, collections.Iterable):
             raise TypeError('value must not be iterable')
-        def round_coordinate(coordinate, precision):
+        def round_coordinate(coordinate):
             return float(Decimal(str(coordinate)).quantize(Decimal(precision_str), rounding=ROUND_HALF_UP))
         precision_str = '.{:0>{prec}d}'.format(1,prec=precision)
-        self.coordinates = tuple([round_coordinate(coordinate, precision) for coordinate in self.coordinates])
+        self.coordinates = tuple([round_coordinate(coordinate) for coordinate in self.coordinates])
 
     def __str__(self):
         return str(self.coordinates)
--- test/test_vector.py
+++ test/test_vector.py
@@ -1,6 +1,13 @@
 import unittest
+from decimal import Decimal, ROUND_HALF_UP
 from linear_algebra.vector import Vector 
 
+def round_float(coordinate, precision):
+    """Simple helper function to round values just like
+        the Vector method round_coordinate does
+    """
+    precision_str = '.{:0>{prec}d}'.format(1,prec=precision)
+    return float(Decimal(str(coordinate)).quantize(Decimal(precision_str), rounding=ROUND_HALF_UP))
 class VectorTestCase(unittest.TestCase):
     def setUp(self):
         self.vector1d = Vector((1,))
@@ -9,68 +16,119 @@ class VectorTestCase(unittest.TestCase):
     def test_init_simple_case(self):
         self.assertEqual(self.vector3d.coordinates, (1,2,3), 'init does not work-coordinates incorrect')
         self.assertEqual(self.vector3d.dimension, 3, 'init does not work-dimension incorrect')
+
     def test_init_one_dimension(self):
         self.assertEqual(self.vector1d.coordinates, (1,),'init does not work-coordinates incorrect')
         self.assertEqual(self.vector1d.dimension, 1, 'init does not work-dimension incorrect')
+
     def test_init_empty_tuple(self):
         with self.assertRaises(ValueError, msg='init does not throw ValueError if coordinates are empty'):
                 Vector( () )
+    
     def test_init_not_iterable(self):
         with self.assertRaises(TypeError, msg='init does not throw TypeError if coordinates are not iterable'):
                 Vector( 1 )
+    
     def test_str_single_dimension(self):
         self.assertEqual(str(self.vector1d), "(1,)",'str does not work')
+    
     def test_str_three_dimensions(self):
         self.assertEqual(str(self.vector3d), "(1, 2, 3)",'str does not work')
+    
     def test_eq_three_dimensions(self):
         self.assertEqual(self.vector3d==Vector((1,2,3)), True,'eq does not work')
         self.assertEqual(Vector((-1,2,3))==self.vector3d, False,'eq does not work')
+    
     def test_eq_one_dimension(self):
         self.assertEqual(self.vector1d==Vector((1,)), True, 'eq does not work')
         self.assertEqual(self.vector1d==Vector((-1,)), False, 'eq does not work')
+    
     def test_round_coordinates(self):
         vector = Vector((1.00001, 2.12348, -8274.125, 2))
         vector.round_coordinates(2)
         self.assertEqual(vector.coordinates, (1.00, 2.12, -8274.13, 2.00), 'round_coordinates does not work')
+    
     def test_round_coordinates_assert(self):
         with self.assertRaises(TypeError, msg='round_coordinates does not throw TypeError if precision is iterable'):
                 self.vector3d.round_coordinates((1,2,3))
+    
     def test_plus_1d(self):
         self.assertEqual(self.vector1d.plus(Vector((-1,))), Vector((0,)), 'plus does not work')
         self.assertEqual(self.vector1d, Vector((1,)), 'plus does not work - changes vector')
+    
     def test_plus_3d(self):
         self.assertEqual(self.vector3d.plus(Vector((-1,2,3))), Vector((0,4,6)), 'plus does not work')
         self.assertEqual(self.vector3d, Vector((1,2,3)), 'plus does not work - changes vector')
+    
     def test_minus_1d(self):
         self.assertEqual(self.vector1d.minus(Vector((-1,))), Vector((2,)), 'minus does not work')
         self.assertEqual(self.vector1d, Vector((1,)), 'plus does not work - changes vector')
+    
     def test_minus_1d(self):
         self.assertEqual(self.vector3d.minus(Vector((-1,2,3))), Vector((2,0,0)), 'minus does not work')
         self.assertEqual(self.vector3d, Vector((1,2,3)), 'plus does not work - changes vector')
+    
     def test_scalar_multiplication_1d(self):
         self.assertEqual(self.vector1d.scalar_multiply(-1), Vector((-1,)), 'scalar multiply does not work')
         self.assertEqual(self.vector1d, Vector((1,)), 'scalar multiply does not work - changes vector')
+    
     def test_scalar_multiplication_3d(self):
         self.assertEqual(self.vector3d.scalar_multiply(-1), Vector((-1,-2,-3)), 'scalar multiply does not work')
         self.assertEqual(self.vector3d, Vector((1,2,3)), 'scalar multiply does not work - changes vector')
+    
     def test_scalar_multiply_assert(self):
         with self.assertRaises(TypeError, msg='scalar_multiply does not throw TypeError if scalar is iterable'):
-                self.vector3d.scalar_multiply((1,2,3))
+            self.vector3d.scalar_multiply((1,2,3))
+    
     def test_plus_float(self):
         v1 = Vector((8.218, -9.341))
         v2 = Vector((-1.129, 2.111))
         resultV = v1.plus(v2)
         resultV.round_coordinates(3)
         self.assertEqual(resultV, Vector((7.089,-7.230)), 'plus does not work')
+    
     def test_minus_float(self):
         v1 = Vector((7.119, 8.215))
         v2 = Vector((-8.223, 0.878))
         resultV = v1.minus(v2)
         resultV.round_coordinates(3)
         self.assertEqual(resultV, Vector((15.342,7.337)), 'minus does not work')
+    
     def test_scalar_multiply_float(self):
         v1 = Vector((1.671, -1.012, -0.318))
         scalar = 7.41
         resultV = v1.scalar_multiply(scalar)
         resultV.round_coordinates(3)
         self.assertEqual(resultV, Vector((12.382,-7.499,-2.356)), 'scalar multiply does not work')
+    
+    def test_magnitude_simple(self):
+        result = Vector((2,2,2,2)).magnitude()
+        self.assertEqual(result, 4, 'magnitude does not work')
+    
+    def test_magnitude_float_2d(self):
+        result = Vector((-0.221, 7.437)).magnitude()
+        self.assertEqual(round_float(result, 3), 7.440, 'magnitude does not work')
+    
+    def test_magnitude_float_3d(self):
+        result = Vector((8.813, -1.331, -6.247)).magnitude()
+        self.assertEqual(round_float(result, 3),10.884, 'magnitude does not work')
+    
+    def test_direction_simple(self):
+        result = Vector((2,2,2,2)).direction()
+        self.assertEqual(result.coordinates, (.5,.5,.5,.5), 'direction does not work')
+    
+    def test_direction_assert(self):
+        with self.assertRaises(ZeroDivisionError, msg='direction does not throw ZeroDivisionError if magnitude is 0'):
+            Vector((0,0,0)).direction()
+    
+    def test_direction_float_2d(self):
+        result = Vector((5.581, -2.136)).direction()
+        #5.976
+        result.round_coordinates(3)
+        self.assertEqual(result.coordinates, (0.934, -0.357), 'direction does not work')
+    
+    def test_direction_float_3d(self):
+        result = Vector((1.996, 3.108, -4.554)).direction()
+        #5.864
+        result.round_coordinates(3)
+        self.assertEqual(result.coordinates, (0.340,0.530,-0.777), 'direction does not work')
