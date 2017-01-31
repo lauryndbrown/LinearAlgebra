3beca099e5131cb167d20356af88fa1308bdb042
lauryndbrown <lauryndbrown@gmail.com>
2017-01-30 21:40:31 -0600

    Added plus, minus, and scalar multiply

--- linear_algebra/vector.py
+++ linear_algebra/vector.py
@@ -1,3 +1,5 @@
+import collections
+from decimal import Decimal
 class Vector:
     """
 
@@ -12,6 +14,22 @@ class Vector:
             raise ValueError('The coordinates must be nonempty')
         except TypeError:
             raise TypeError('The coordinates must be iterable')
+    def plus(self, vector):
+        sums = tuple([ float(Decimal(str(a))+Decimal(str(b))) for a, b in zip(self.coordinates, vector.coordinates) ])
+        return Vector(sums)
+        
+    def minus(self, vector):
+        differences = tuple([float(Decimal(str(a))-Decimal(str(b))) for a, b in zip(self.coordinates, vector.coordinates) ])
+        return Vector(differences)
+
+    def scalar_multiply(self, scalar):
+        if isinstance(scalar, collections.Iterable):
+            raise TypeError('value must not be iterable')
+        products = tuple([float(Decimal(str(coordinate))*Decimal(str(scalar))) for coordinate in self.coordinates ])
+        return Vector(products)
+
+    def round_coordinates(self, num_decimals):
+        self.coordinates = tuple([round(coordinate, num_decimals) for coordinate in self.coordinates])
     def __str__(self):
         return str(self.coordinates)
     def __eq__(self, other):
--- test/test_vector.py
+++ test/test_vector.py
@@ -3,15 +3,15 @@ from linear_algebra.vector import Vector
 
 class VectorTestCase(unittest.TestCase):
     def setUp(self):
-        pass
+        self.vector1d = Vector((1,))
+        self.vector3d = Vector((1,2,3))
+
     def test_init_simple_case(self):
-        vector = Vector((1,2,3))
-        self.assertEqual(vector.coordinates, (1,2,3), 'init does not work-coordinates incorrect')
-        self.assertEqual(vector.dimension, 3, 'init does not work-dimension incorrect')
+        self.assertEqual(self.vector3d.coordinates, (1,2,3), 'init does not work-coordinates incorrect')
+        self.assertEqual(self.vector3d.dimension, 3, 'init does not work-dimension incorrect')
     def test_init_one_dimension(self):
-        vector = Vector((1, ))
-        self.assertEqual(vector.coordinates, (1,),'init does not work-coordinates incorrect')
-        self.assertEqual(vector.dimension, 1, 'init does not work-dimension incorrect')
+        self.assertEqual(self.vector1d.coordinates, (1,),'init does not work-coordinates incorrect')
+        self.assertEqual(self.vector1d.dimension, 1, 'init does not work-dimension incorrect')
     def test_init_empty_tuple(self):
         with self.assertRaises(ValueError, msg='init does not throw ValueError if coordinates are empty'):
                 Vector( () )
@@ -19,14 +19,51 @@ class VectorTestCase(unittest.TestCase):
         with self.assertRaises(TypeError, msg='init does not throw TypeError if coordinates are not iterable'):
                 Vector( 1 )
     def test_str_single_dimension(self):
-        vector = Vector((1, ))
-        self.assertEqual(str(vector), "(1,)",'str does not work')
+        self.assertEqual(str(self.vector1d), "(1,)",'str does not work')
     def test_str_three_dimensions(self):
-        vector = Vector((1,2,3 ))
-        self.assertEqual(str(vector), "(1, 2, 3)",'str does not work')
+        self.assertEqual(str(self.vector3d), "(1, 2, 3)",'str does not work')
     def test_eq_three_dimensions(self):
-        self.assertEqual(Vector((1,2,3))==Vector((1,2,3)), True,'eq does not work')
-        self.assertEqual(Vector((-1,2,3))==Vector((1,2,3)), False,'eq does not work')
+        self.assertEqual(self.vector3d==Vector((1,2,3)), True,'eq does not work')
+        self.assertEqual(Vector((-1,2,3))==self.vector3d, False,'eq does not work')
     def test_eq_one_dimension(self):
-        self.assertEqual(Vector((1,))==Vector((1,)), True, 'eq does not work')
-        self.assertEqual(Vector((1,))==Vector((-1,)), False, 'eq does not work')
+        self.assertEqual(self.vector1d==Vector((1,)), True, 'eq does not work')
+        self.assertEqual(self.vector1d==Vector((-1,)), False, 'eq does not work')
+    def test_plus_1d(self):
+        self.assertEqual(self.vector1d.plus(Vector((-1,))), Vector((0,)), 'plus does not work')
+        self.assertEqual(self.vector1d, Vector((1,)), 'plus does not work - changes vector')
+    def test_plus_3d(self):
+        self.assertEqual(self.vector3d.plus(Vector((-1,2,3))), Vector((0,4,6)), 'plus does not work')
+        self.assertEqual(self.vector3d, Vector((1,2,3)), 'plus does not work - changes vector')
+    def test_minus_1d(self):
+        self.assertEqual(self.vector1d.minus(Vector((-1,))), Vector((2,)), 'minus does not work')
+        self.assertEqual(self.vector1d, Vector((1,)), 'plus does not work - changes vector')
+    def test_minus_1d(self):
+        self.assertEqual(self.vector3d.minus(Vector((-1,2,3))), Vector((2,0,0)), 'minus does not work')
+        self.assertEqual(self.vector3d, Vector((1,2,3)), 'plus does not work - changes vector')
+    def test_scalar_multiplication_1d(self):
+        self.assertEqual(self.vector1d.scalar_multiply(-1), Vector((-1,)), 'scalar multiply does not work')
+        self.assertEqual(self.vector1d, Vector((1,)), 'scalar multiply does not work - changes vector')
+    def test_scalar_multiplication_3d(self):
+        self.assertEqual(self.vector3d.scalar_multiply(-1), Vector((-1,-2,-3)), 'scalar multiply does not work')
+        self.assertEqual(self.vector3d, Vector((1,2,3)), 'scalar multiply does not work - changes vector')
+    def test_scalar_multiply_assert(self):
+        with self.assertRaises(TypeError, msg='scalar_multiply does not throw TypeError if scalar is iterable'):
+                self.vector3d.scalar_multiply((1,2,3))
+    def test_plus_float(self):
+        v1 = Vector((8.218, -9.341))
+        v2 = Vector((-1.129, 2.111))
+        resultV = v1.plus(v2)
+        resultV.round_coordinates(3)
+        self.assertEqual(resultV, Vector((7.089,-7.230)), 'plus does not work')
+    def test_minus_float(self):
+        v1 = Vector((7.119, 8.215))
+        v2 = Vector((-8.223, 0.878))
+        resultV = v1.minus(v2)
+        resultV.round_coordinates(3)
+        self.assertEqual(resultV, Vector((15.342,7.337)), 'minus does not work')
+    def test_scalar_multiply_float(self):
+        v1 = Vector((1.671, -1.012, -0.318))
+        scalar = 7.41
+        resultV = v1.scalar_multiply(scalar)
+        resultV.round_coordinates(3)
+        self.assertEqual(resultV, Vector((12.382,-7.499,-2.356)), 'scalar multiply does not work')
